62930e7d8839c1104e3452e4490685e793fdbc0b
FidelCoder7 <fidelokemwa211@gmail.com>
2026-06-22 13:54:34 +0300

    feat: add protected/admin route guards and wire up app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-
-import './App.css'
-
-
-
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
